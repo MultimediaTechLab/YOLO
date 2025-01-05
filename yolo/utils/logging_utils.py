41f207692eead7eb276dd17ccd335123f4a6b9ae
joncrall <erotemic@gmail.com>
2025-01-05 13:22:00 -0500

    change: other deterministic disable

--- yolo/utils/logging_utils.py
+++ yolo/utils/logging_utils.py
@@ -48,7 +48,7 @@ def set_seed(seed):
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
-    torch.backends.cudnn.deterministic = True
+    # torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
 
